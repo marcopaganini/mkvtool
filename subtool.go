0d05cdf73c78bfbbfd09872576fcc9280c6180f7
Marco Paganini <paganini@paganini.net>
2019-12-21 18:24:54 -0800

    Add "only" command and other changes.
    - Add "only" command to remove all subtitle tracks but one.
    - All references to tracks now happen by track num, not track id.
    - All track numbers printed use the mkvmerge format (firt = 0).

--- subtool.go
+++ subtool.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jedib0t/go-pretty/table"
 	"github.com/remko/go-mkvparse"
 	"gopkg.in/alecthomas/kingpin.v2"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
@@ -31,9 +32,16 @@ type trackinfo struct {
 	CodecID     string
 }
 
+// trackFileInfo holds information about an exported track file.
+type trackFileInfo struct {
+	language string
+	fname    string
+}
+
 type MyParser struct {
 	track   trackinfo
 	tracks  []trackinfo
+	fname   string
 	inTrack bool
 }
 
@@ -114,14 +122,15 @@ func show(p MyParser) {
 	tab.SetOutputMirror(os.Stdout)
 	tab.AppendHeader(table.Row{"Number", "UID", "Type", "Name", "Language", "Codec", "Default"})
 
+	// Commands use track starting at offset zero, hence the subtraction below.
 	for _, t := range p.tracks {
-		tab.AppendRow([]interface{}{t.number, t.uid, t.tracktype, t.name, t.language, t.CodecID, t.flagDefault})
+		tab.AppendRow([]interface{}{t.number - 1, t.uid, t.tracktype, t.name, t.language, t.CodecID, t.flagDefault})
 	}
 	tab.Render()
 }
 
 // setdefault resets flagDefault on all subtitle tracks and sets it on the chosen track UID.
-func setdefault(mkvfile string, p MyParser, trackUID int64, cmd runner) error {
+func setdefault(mkvfile string, p MyParser, track int64, cmd runner) error {
 	command := []string{
 		"mkvpropedit",
 		mkvfile,
@@ -136,18 +145,64 @@ func setdefault(mkvfile string, p MyParser, trackUID int64, cmd runner) error {
 	if err := cmd.run(command[0], command[1:]...); err != nil {
 		return err
 	}
-	return adddefault(mkvfile, trackUID, cmd)
+	return adddefault(mkvfile, track, cmd)
+}
+
+// extract extracts a given track into a file.
+func extract(handler MyParser, track int64, cmd runner) (trackFileInfo, error) {
+	// Fetch language for the track. Fail if track does not exist.
+	ti, err := trackInfo(handler, track)
+	if err != nil {
+		return trackFileInfo{}, err
+	}
+
+	// Extract into a temporary file
+	tmpfile, err := ioutil.TempFile("", "subtool")
+	if err != nil {
+		return trackFileInfo{}, err
+	}
+	temp := tmpfile.Name()
+	tmpfile.Close()
+
+	command := []string{
+		"mkvextract",
+		handler.fname,
+		"tracks",
+		fmt.Sprintf("%d:%s", track, temp),
+	}
+	if err := cmd.run(command[0], command[1:]...); err != nil {
+		return trackFileInfo{}, err
+	}
+	return trackFileInfo{language: ti.language, fname: temp}, nil
+}
+
+// mux merges an input file (usually an mkv file) and multiple subtitles into a
+// destination, optionally removing all other subtitles from the source.
+func submux(infile, outfile string, nosubs bool, cmd runner, subs ...trackFileInfo) error {
+	cmdline := []string{"mkvmerge", "-o", outfile}
+
+	if nosubs {
+		cmdline = append(cmdline, "-S")
+	}
+	cmdline = append(cmdline, infile)
+
+	for track, sub := range subs {
+		cmdline = append(cmdline, "--language", fmt.Sprintf("%d:%s", track, sub.language))
+		cmdline = append(cmdline, sub.fname)
+	}
+	return cmd.run(cmdline[0], cmdline[1:]...)
+
 }
 
 // adddefault adds the default flag to a given track UID.
-func adddefault(mkvfile string, trackUID int64, cmd runner) error {
-	return cmd.run("mkvpropedit", mkvfile, "--edit", fmt.Sprintf("track:=%d", trackUID), "--set", "flag-default=1")
+func adddefault(mkvfile string, track int64, cmd runner) error {
+	return cmd.run("mkvpropedit", mkvfile, "--edit", fmt.Sprintf("track:%d", track), "--set", "flag-default=1")
 }
 
 // mustParseFile parses the MKV file and returns a handler, or aborts with an
 // error message in case of problems.
 func mustParseFile(fname string) MyParser {
-	handler := MyParser{}
+	handler := MyParser{fname: fname}
 	err := mkvparse.ParsePath(fname, &handler)
 	if err != nil {
 		log.Fatal(err)
@@ -155,25 +210,43 @@ func mustParseFile(fname string) MyParser {
 	return handler
 }
 
+// trackInfo returns the trackinfo for a given track number, or error if it does not exist.
+func trackInfo(handler MyParser, track int64) (trackinfo, error) {
+	for _, v := range handler.tracks {
+		if v.number == track {
+			return v, nil
+		}
+	}
+	return trackinfo{}, fmt.Errorf("track number %d not found in file %s\n", track, handler.fname)
+}
+
 func main() {
 	var (
 		app    = kingpin.New("subtool", "Subtitle operations on matroska containers")
 		dryrun = app.Flag("dry-run", "Dry-run mode (only show commands)").Short('n').Bool()
 
-		//debug = app.Flag("debug", "Enable debug mode.").Bool()
 		// show
 		showCmd  = app.Command("show", "Show Information about a file.")
 		showFile = showCmd.Arg("input-file", "Matroska Input file").Required().String()
 
 		// setdefault
-		setDefaultCmd  = app.Command("setdefault", "Set default subtitle tag")
-		setDefaultFile = setDefaultCmd.Arg("input-file", "Matroska Input file").Required().String()
-		trackUID       = setDefaultCmd.Arg("trackUID", "Track UID to set as default").Required().Int64()
+		setDefaultCmd   = app.Command("setdefault", "Set default subtitle tag")
+		setDefaultFile  = setDefaultCmd.Arg("input-file", "Matroska Input file").Required().String()
+		setDefaultTrack = setDefaultCmd.Arg("track", "Track number to set as default").Required().Int64()
 
-		// Command runners.
-		runCmd     runCommand
+		// only
+		setOnlyCmd    = app.Command("only", "Remove all subtitle tracks, except one")
+		setOnlyTrack  = setOnlyCmd.Arg("track", "Track number to keep").Required().Int64()
+		setOnlyInput  = setOnlyCmd.Arg("input", "Matroska Input file").Required().String()
+		setOnlyOutput = setOnlyCmd.Arg("output", "Matroska Output file").Required().String()
+
+		// Command runner.
+		runCmd runCommand
+
+		// Dry-run command runner (only print commands).
 		fakeRunCmd fakeRunCommand
-		run        runner
+
+		run runner
 	)
 
 	// Plain logs.
@@ -193,8 +266,19 @@ func main() {
 		show(h)
 	case setDefaultCmd.FullCommand():
 		h := mustParseFile(*setDefaultFile)
-		if err := setdefault(*setDefaultFile, h, *trackUID, run); err != nil {
+		if err := setdefault(*setDefaultFile, h, *setDefaultTrack, run); err != nil {
 			log.Fatal(err)
 		}
+	case setOnlyCmd.FullCommand():
+		h := mustParseFile(*setOnlyInput)
+		tfi, err := extract(h, *setOnlyTrack, run)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = submux(*setOnlyInput, *setOnlyOutput, true, run, tfi)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Remove(tfi.fname)
 	}
 }
